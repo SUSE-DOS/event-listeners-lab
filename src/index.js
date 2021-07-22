38499ce626d1541094b3db9a335823309e848dce
AlokHimself <alokhiself@gmail.com>
2021-07-22 20:36:00 +0100

    lab

---
+++ src/index.js
@@ -0,0 +1,95 @@
+"use strict";
+
+/**
+ Problem #1: addEventListener practice
+
+  Under the `src` directory, create files `index1.js` and `index1.html`. Link `js` file to the `html` file, and test if it's working correctly.
+  Using the following HTML mark up, your goal is to add click listner to each button `hello` and `goodbye`
+
+  1. When the hello button is clicked, you should console.log "hello"
+  2. When the goodbye button is clicked, you should console.log "goodbye"
+
+      <!DOCTYPE html>
+
+          <head>
+              <title>ClasList</title>
+              <!--LEAVE THESE LINES ALONE, PLEASE! -->
+
+          </head>
+
+          <body>
+              <h1>Click These Buttons</h1>
+              <p>And Prepare To Have Your Mind Blown</p>
+              <button id="hello">Hello</button>
+              <button id="goodbye">Goodbye</button>
+          </body>
+
+      </html>
+
+
+ */
+
+// ANSWER
+
+
+/**
+ Problem #2: addEventListener practice
+
+  Under the `src` directory, create files `index2.js` and `index2.html`. Link `js` file to the `html` file, and test if it's working correctly.
+  Using the following HTML mark up, your goal is to add click listner to the button with id `try` so that.
+
+  1. When the Click here button is clicked, <h1> should display the text "Amazing!!"
+
+
+  <!DOCTYPE html>
+
+
+        <head>
+            <title>ClasList</title>
+            <!--LEAVE THESE LINES ALONE, PLEASE! -->
+
+        </head>
+
+
+        <body>
+        <button id="try">Click here</button>
+        <h1 id="text"></h1>
+        </body>
+
+      </html>
+ */
+
+// ANSWER
+
+
+
+
+/**
+ Problem #3: Input Event Practice
+
+  Under the `src` directory, create files `index3.js` and `index3.html`. Link `js` file to the `html` file, and test if it's working correctly.
+  In the following HTML mark up, you will find an <h1> and an <input type ="text"> element. Your goal is to write a code that meets the following requirements.
+
+
+   1. Whenever an `input event` is fired on the <input> element, update the <h1> so that it displays "Welcome", plus the current from the text input.
+   2. If the <input> goes back to being empty, update the <h1> so that it once again says "Enter Your Username"
+
+   Please do not change any element of this HTML markup
+
+           <!DOCTYPE html>
+
+             <head>
+                 <title>Input Event</title>
+                 <!--LEAVE THESE LINES ALONE, PLEASE! -->
+             </head>
+
+             <body>
+                 <h1>Enter Your Username</h1>
+                 <input type="text" id="username">
+             </body>
+
+         </html>
+
+          */
+
+// ANSWER
